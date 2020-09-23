f4b249ce7207ac865e9a83c29a519c8ef3e3c3e3
Darrian DeVaughnn <devondevaughnn@gmail.com>
2020-09-22 20:56:33 -0700

    unsuccessfully attempted to fix bug causing 'undefined' from popping up

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var currentDay = moment().format('1');
 //Create Current day at top of page
 $("#currentDay").text(today);
 var currentTime = Number(moment().format('H'));
-TODO:// Create an Array to Save Events
+
 var eventArray = [];
 var todo = {
     todoTime: " ",
@@ -20,20 +20,18 @@ todayEvents();
 function todayEvents() {
     var hours = ["9 am ","10 am","11 am","12 pm","1 pm","2 pm","3 pm","4 pm","5 pm"]
 for (var i = 0; i < hours.length ; i++) {   
-    var startDay = hours[i] + (timeOfDay);
+    var startDay = (hours[i]) + (timeOfDay);
     //Create Rows for the events
     var hour = $("<section>").addClass("row time-block");
     //Create column for the hour
     var time = $("<div>").addClass("col-md-2 hour font-weight-bold");
     //Creates the text area
-    var textArea = $("<textarea>").addClass("col-md-8 description").attr("id", i + (timeOfDay-1));
+    var textArea = $("<textarea>").addClass("col-md-8 description").attr("id", i + (timeOfDay));
     //Create the save button
     var save = $("<button>").addClass("col-md-1 saveBtn");
     //Create icon
     var icon = $("<i>").addClass("far fa-save").attr( "i", "hover");
     var timeOfDay = " ";
-    
- 
     //Create color code for past/present/future
     if (i < currentTime) {
         textArea.addClass("past");
@@ -44,15 +42,13 @@ for (var i = 0; i < hours.length ; i++) {
     else {
         textArea.addClass("present");
     }
-    
     //Appending each element to the page
     save.append(icon);
     time.text(startDay);
     hour.append(time);
     hour.append(textArea);
     hour.append(save);
-    
-    $(".container").append(hour);
+   $(".container").append(hour);
 
 }}
 
